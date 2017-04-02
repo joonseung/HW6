4c8644f26c9555571f7b74cc86bd22350d16b712
Joonseung Rhee <joonseung-rhee@uiowa.edu>
2017-04-02 01:03:38 -0500

    searchQuery.java done

--- HW6/src/java/dbHelpers/SearchQuery.java
+++ HW6/src/java/dbHelpers/SearchQuery.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dbHelpers;
 
 import java.io.IOException;
@@ -10,34 +5,35 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet; 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.lolChampions;
+
 /**
  *
  * @author Abc
  */
-public class ReadQuery {
-    private Connection conn;
-    private ResultSet results;
+public class SearchQuery {
     
-    public ReadQuery() {
-        
-    try{
+ private Connection conn;
+ private ResultSet results;
+ 
+ public SearchQuery(){
+ try{
             Properties props = new Properties(); 
             InputStream instr = getClass().getResourceAsStream("dbConn.properties");
             try {
                 props.load(instr);
             } catch (IOException ex) {
-                Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(SearchQuery.class.getName()).log(Level.SEVERE, null, ex);
             }
             try {
                 instr.close();
             } catch (IOException ex) {
-                Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(SearchQuery.class.getName()).log(Level.SEVERE, null, ex);
             }
             String driver = props.getProperty("driver.name");
             String url = props.getProperty("server.name");
@@ -46,28 +42,28 @@ public class ReadQuery {
             Class.forName(driver);
             conn = DriverManager.getConnection(url, username, passwd);
             } catch (ClassNotFoundException ex) {
-                Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(SearchQuery.class.getName()).log(Level.SEVERE, null, ex);
             } catch (SQLException ex) {
-                Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(SearchQuery.class.getName()).log(Level.SEVERE, null, ex);
             }
+ 
+ }
     
-    }
-    
-    public void doRead(){
+ public void doSearch(String champName){
+ 
+     try {
+         String query = "SELECT * FROM lolChampions WHERE champName LIKE ?";
+         
+         PreparedStatement ps = conn.prepareStatement(query);
+         ps.setString(1,"%" + champName + "%");
+         this.results = ps.executeQuery();
+     } catch (SQLException ex) {
+         Logger.getLogger(SearchQuery.class.getName()).log(Level.SEVERE, null, ex);
+     }
         
-        try {
-            String query = "Select * from lolChampions";
-            
-            PreparedStatement ps = conn.prepareStatement(query);
-            this.results = ps.executeQuery();
-            
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public String getHTMLTable(){
+ }
+ 
+ public String getHTMLTable(){
         
         String table = "";
         
